593511190cd0b400a13f671ac949c818931f8ce8
hakanakkuss <hakannakkus6@gmail.com>
2024-02-27 19:49:14 +0300

    changes from previous project transferred to this project

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,35 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="contentBg">
+    <navigation-bar />
+    <router-view></router-view>
+    <div class="bg-gray-700 w-full h-40 fixed bottom-0">
+  <span class="flex items-center justify-center h-full">
+    Footer
+  </span>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import HomePage from '@/components/home-page.vue';
+import NavigationBar from '@/shared/navigation-bar.vue'
+import FooterPage from '@/shared/footer-page.vue';
+import FavoriteStocks from '@/components/favorite-stocks.vue'
 export default {
-  name: 'App',
   components: {
-    HelloWorld
+    HomePage,
+    NavigationBar,
+    FooterPage,
+    FavoriteStocks
   }
-}
+};
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<!--<style>-->
+<!--/*.contentBg {*/-->
+<!--/*  background: linear-gradient(to right, rgba(245, 245, 245, .8), rgba(245, 245, 245, .8)), url(src/assets/common-bg.svg);*/-->
+<!--/*  height: 100vh;*/-->
+<!--/*  min-height: 73rem;*/-->
+<!--/*  position: relative;*/-->
+<!--//}-->
+<!--</style>-->
--- src/main.js
+++ src/main.js
@@ -1,4 +1,18 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from 'vue';
+import App from './App.vue';
+import router from '@/router';
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+import * as ElementPlusIconsVue from '@element-plus/icons-vue'
+import store from "../store";
+import "../src/assets/input.css"
 
-createApp(App).mount('#app')
+
+const app = createApp(App);
+app.use(router);
+app.use(ElementPlus);
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+    app.component(key, component)
+}
+app.use(store);
+app.mount('#app');
